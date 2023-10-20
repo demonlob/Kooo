3351d92b85a70f1c9a841f052e6207eeed81af7e
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2023-10-21 00:52:09 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -11,6 +11,7 @@
 import os
 import re
 import textwrap
+import rndom
 
 import aiofiles
 import aiohttp
@@ -20,6 +21,73 @@ from youtubesearchpython.__future__ import VideosSearch
 
 from config import MUSIC_BOT_NAME, YOUTUBE_IMG_URL
 
+themes = [
+    "green",
+    "orange",
+    "purple",
+    "yellow",
+]
+
+async def check_theme():
+    theme = random.choice(themes)
+    return theme
+
+async def specialfont_to_normal(ctitle):
+    string = ctitle
+    font1 = list("𝔄𝔅ℭ𝔇𝔈𝔉𝔊ℌℑ𝔍𝔎𝔏𝔐𝔑𝔒𝔓𝔔ℜ𝔖𝔗𝔘𝔙𝔚𝔛𝔜ℨ")
+    font2 = list("𝕬𝕭𝕮𝕯𝕰𝕱𝕲𝕳𝕴𝕵𝕶𝕷𝕸𝕹𝕺𝕻𝕼𝕽𝕾𝕿𝖀𝖁𝖂𝖃𝖄𝖅")
+    font3 = list("𝓐𝓑𝓒𝓓𝓔𝓕𝓖𝓗𝓘𝓙𝓚𝓛𝓜𝓝𝓞𝓟𝓠𝓡𝓢𝓣𝓤𝓥𝓦𝓧𝓨𝓩")
+    font4 = list("𝒜𝐵𝒞𝒟𝐸𝐹𝒢𝐻𝐼𝒥𝒦𝐿𝑀𝒩𝒪𝒫𝒬𝑅𝒮𝒯𝒰𝒱𝒲𝒳𝒴𝒵")
+    font5 = list("𝔸𝔹ℂ𝔻𝔼𝔽𝔾ℍ𝕀𝕁𝕂𝕃𝕄ℕ𝕆ℙℚℝ𝕊𝕋𝕌𝕍𝕎𝕏𝕐ℤ")
+    font6 = list("ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ")
+    font26 = list("𝐀𝐁𝐂𝐃𝐄𝐅𝐆𝐇𝐈𝐉𝐊𝐋𝐌𝐍𝐎𝐏𝐐𝐑𝐒𝐓𝐔𝐕𝐖𝐗𝐘𝐙")
+    font27 = list("𝗔𝗕𝗖𝗗𝗘𝗙𝗚𝗛𝗜𝗝𝗞𝗟𝗠𝗡𝗢𝗣𝗤𝗥𝗦𝗧𝗨𝗩𝗪𝗫𝗬𝗭")
+    font28 = list("𝘈𝘉𝘊𝘋𝘌𝘍𝘎𝘏𝘐𝘑𝘒𝘓𝘔𝘕𝘖𝘗𝘘𝘙𝘚𝘛𝘜𝘝𝘞𝘟𝘠𝘡")
+    font29 = list("𝘼𝘽𝘾𝘿𝙀𝙁𝙂𝙃𝙄𝙅𝙆𝙇𝙈𝙉𝙊𝙋𝙌𝙍𝙎𝙏𝙐𝙑𝙒𝙓𝙔𝙕")
+    font30 = list("𝙰𝙱𝙲𝙳𝙴𝙵𝙶𝙷𝙸𝙹𝙺𝙻𝙼𝙽𝙾𝙿𝚀𝚁𝚂𝚃𝚄𝚅𝚆𝚇𝚈𝚉")    
+    font32 = list("ABCDEFGHIJKₗMNOPQRSTUVWXYZ")
+    font1L = list("𝔞𝔟𝔠𝔡𝔢𝔣𝔤𝔥𝔦𝔧𝔨𝔩𝔪𝔫𝔬𝔭𝔮𝔯𝔰𝔱𝔲𝔳𝔴𝔵𝔶𝔷")
+    font2L = list("𝖆𝖇𝖈𝖉𝖊𝖋𝖌𝖍𝖎𝖏𝖐𝖑𝖒𝖓𝖔𝖕𝖖𝖗𝖘𝖙𝖚𝖛𝖜𝖝𝖞𝖟")
+    font3L = list("𝓪𝓫𝓬𝓭𝓮𝓯𝓰𝓱𝓲𝓳𝓴𝓵𝓶𝓷𝓸𝓹𝓺𝓻𝓼𝓽𝓾𝓿𝔀𝔁𝔂𝔃")
+    font4L = list("𝒶𝒷𝒸𝒹𝑒𝒻𝑔𝒽𝒾𝒿𝓀𝓁𝓂𝓃𝑜𝓅𝓆𝓇𝓈𝓉𝓊𝓋𝓌𝓍𝓎𝓏")
+    font5L = list("𝕒𝕓𝕔𝕕𝕖𝕗𝕘𝕙𝕚𝕛𝕜𝕝𝕞𝕟𝕠𝕡𝕢𝕣𝕤𝕥𝕦𝕧𝕨𝕩𝕪𝕫")
+    font6L = list("ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ")
+    font27L = list("𝐚𝐛𝐜𝐝𝐞𝐟𝐠𝐡𝐢𝐣𝐤𝐥𝐦𝐧𝐨𝐩𝐪𝐫𝐬𝐭𝐮𝐯𝐰𝐱𝐲𝐳")
+    font28L = list("𝗮𝗯𝗰𝗱𝗲𝗳𝗴𝗵𝗶𝗷𝗸𝗹𝗺𝗻𝗼𝗽𝗾𝗿𝘀𝘁𝘂𝘃𝘄𝘅𝘆𝘇")
+    font29L = list("𝘢𝘣𝘤𝘥𝘦𝘧𝘨𝘩𝘪𝘫𝘬𝘭𝘮𝘯𝘰𝘱𝘲𝘳𝘴𝘵𝘶𝘷𝘸𝘹𝘺𝘻")
+    font30L = list("𝙖𝙗𝙘𝙙𝙚𝙛𝙜𝙝𝙞𝙟𝙠𝙡𝙢𝙣𝙤𝙥𝙦𝙧𝙨𝙩𝙪𝙫𝙬𝙭𝙮𝙯")
+    font31L = list("𝚊𝚋𝚌𝚍𝚎𝚏𝚐𝚑𝚒𝚓𝚔𝚕𝚖𝚗𝚘𝚙𝚚𝚛𝚜𝚝𝚞𝚟𝚠𝚡𝚢𝚣")
+    font32L = list("ᴀʙᴄᴅᴇғɢʜɪᴊᴋʟᴍɴᴏᴘǫrsᴛᴜᴠᴡxʏᴢ")
+    normal = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    normalL = list("abcdefghijklmnopqrstuvwxyz")
+    cout = 0
+    for XCB in font1:
+        string = string.replace(font1[cout],normal[cout])
+        string = string.replace(font2[cout],normal[cout])
+        string = string.replace(font3[cout],normal[cout])
+        string = string.replace(font4[cout],normal[cout])
+        string = string.replace(font5[cout],normal[cout])
+        string = string.replace(font6[cout],normal[cout])
+        string = string.replace(font26[cout],normal[cout])
+        string = string.replace(font27[cout],normal[cout])
+        string = string.replace(font28[cout],normal[cout])
+        string = string.replace(font29[cout],normal[cout])
+        string = string.replace(font30[cout],normal[cout])
+        string = string.replace(font32[cout],normal[cout])
+        string = string.replace(font1L[cout],normalL[cout])
+        string = string.replace(font2L[cout],normalL[cout])
+        string = string.replace(font3L[cout],normalL[cout])
+        string = string.replace(font4L[cout],normalL[cout])
+        string = string.replace(font5L[cout],normalL[cout])
+        string = string.replace(font6L[cout],normalL[cout])
+        string = string.replace(font27L[cout],normalL[cout])
+        string = string.replace(font28L[cout],normalL[cout])
+        string = string.replace(font29L[cout],normalL[cout])
+        string = string.replace(font30L[cout],normalL[cout])
+        string = string.replace(font31L[cout],normalL[cout])
+        string = string.replace(font32L[cout],normalL[cout])
+        cout += 1
+    return string
 
 def changeImageSize(maxWidth, maxHeight, image):
     widthRatio = maxWidth / image.size[0]
@@ -30,7 +98,7 @@ def changeImageSize(maxWidth, maxHeight, image):
     return newImage
 
 
-async def gen_thumb(videoid):
+async def gen_thumb_old(videoid):
     if os.path.isfile(f"cache/{videoid}.png"):
         return f"cache/{videoid}.png"
 
@@ -57,7 +125,6 @@ async def gen_thumb(videoid):
                 channel = result["channel"]["name"]
             except:
                 channel = "Unknown Channel"
-
         async with aiohttp.ClientSession() as session:
             async with session.get(thumbnail) as resp:
                 if resp.status == 200:
@@ -149,3 +216,31 @@ async def gen_thumb(videoid):
         return f"cache/{videoid}.png"
     except Exception:
         return YOUTUBE_IMG_URL
+
+async def gen_thumb(videoid, thumbnail, title, ctitle):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(thumbnail) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open(f"cache/thumb{videoid}.jpg", mode="wb")
+                await f.write(await resp.read())
+                await f.close()
+    ctitle = await specialfont_to_normal(ctitle)
+    theme = await check_theme()            
+    image1 = Image.open(f"cache/thumb{videoid}.jpg")
+    image2 = Image.open(f"Utils/{theme}.png")
+    image3 = changeImageSize(1280, 720, image1)
+    image4 = changeImageSize(1280, 720, image2)
+    image5 = image3.convert("RGBA")
+    image6 = image4.convert("RGBA")
+    Image.alpha_composite(image5, image6).save(f"cache/temp{videoid}.png")
+    img = Image.open(f"cache/temp{videoid}.png")
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("assets/finalfont.ttf", 55)
+    font2 = ImageFont.truetype("assets/finalfont.ttf", 75)
+    draw.text((25, 528), f"Playing on {ctitle[:10]}...", fill="black", font=font)
+    draw.text((25, 610), f"{title[:20]}...", fill= "black", font=font2)
+    img.save(f"cache/final{videoid}.png")
+    os.remove(f"cache/temp{videoid}.png")
+    os.remove(f"cache/thumb{videoid}.jpg")
+    final = f"cache/final{videoid}.png"
+    return final
