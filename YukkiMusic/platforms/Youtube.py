4bef0f97187c80caf7a7a5e6f431ac83c3132fdc
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2023-10-23 14:35:28 +0530

    fixed yt vplay

--- YukkiMusic/platforms/Youtube.py
+++ YukkiMusic/platforms/Youtube.py
@@ -299,7 +299,7 @@ class YouTubeAPI:
 
         def video_dl():
             ydl_optssx = {
-                "format": "(bestvideo[height<=?2160][width<=?3840][ext=mp4])+(bestaudio[ext=m4a])",
+                'format': f'bestvideo[height<=?2160][width<=?3840]+bestaudio/best',
                 "outtmpl": "downloads/%(id)s.%(ext)s",
                 "geo_bypass": True,
                 "nocheckcertificate": True,
