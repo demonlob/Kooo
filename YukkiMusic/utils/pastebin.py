1010320191a42402cc96039e8f28dacaa8a5ef76
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2023-10-23 14:04:51 +0530

    fixed paste bin

--- YukkiMusic/utils/pastebin.py
+++ YukkiMusic/utils/pastebin.py
@@ -8,15 +8,24 @@
 # All rights reserved.
 #
 
-import aiohttp, asyncio
-from nekobin import Nekobin
+import aiohttp
 
-nb = Nekobin()
+BASE = "https://batbin.me/"
+
+
+async def post(url: str, *args, **kwargs):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, *args, **kwargs) as resp:
+            try:
+                data = await resp.json()
+            except Exception:
+                data = await resp.text()
+        return data
+
 
 async def Yukkibin(text):
-    result = await nb.paste(text)
-    if result.ok:
-        return(result.url)
-    else:
-        print("Nekobin Error:",  result.message)
+    resp = await post(f"{BASE}api/v2/paste", data=text)
+    if not resp["success"]:
         return
+    link = BASE + resp["message"]
+    return link
